d86fcfa45e1749d0947ab31e5af1ccf7654ce3db
William Chong <williamchong007@msn.com>
2019-01-08 12:30:55 +0800

    + Add eslint nuxt plugin ! Auto fix lint warning

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
+      <app-logo />
       <h1 class="title">
         nuxt-ssr-firebase
       </h1>
@@ -12,11 +12,17 @@
         <a
           href="https://nuxtjs.org/"
           target="_blank"
-          class="button--green">Documentation</a>
+          class="button--green"
+        >
+          Documentation
+        </a>
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
-          class="button--grey">GitHub</a>
+          class="button--grey"
+        >
+          GitHub
+        </a>
       </div>
     </div>
   </section>
