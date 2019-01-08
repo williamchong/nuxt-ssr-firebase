db5ddc849d32963111997ac71434b2cc584c398d
William Chong <williamchong007@msn.com>
2019-01-08 12:23:35 +0800

    ! Merge `nuxt.config.js` from src when deploying function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,13 @@
 const functions = require('firebase-functions');
 const { Nuxt } = require('nuxt-edge');
 
+const nuxtConfig = require('./nuxt.config.js');
+
 const config = {
+  ...nuxtConfig,
   dev: false,
   buildDir: 'nuxt',
 };
 const nuxt = new Nuxt(config);
+
 exports.ssrapp = functions.https.onRequest((req, res) => nuxt.render(req, res));
